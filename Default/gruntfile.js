b184f2c0dbe63f5a9b4d93d40a522b739a9ddcea
Matthijs Hoek <81202@glr.nl>
2018-09-10 14:15:49 +0200

    added file structure

---
+++ Default/gruntfile.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = function (grunt) {
+    // Add configuration, tasks and plugins here
+    grunt.initConfig({
+
+        // imports the config data from the package.json
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass:{
+            dist: {
+                files: {
+                    'public/stylesheets/style.css' : 'public/stylesheets/sass/style.scss'
+                }
+            }
+        },
+
+        cssmin:{
+            target: {
+                files: {
+                'public/stylesheets/style.css' : 'public/stylesheets/css/style.css'
+                }
+            }
+        },
+
+        watch:{
+            options: {
+                livereload: false
+            },
+            css: {
+                files: ['**/*.scss'],
+                tasks: ['sass', 'cssmin']
+            },
+        }
+        // Register the default tasks.
+
+    });
+    // Load the Grunt plugins.
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask('default', ['watch']);
+};
